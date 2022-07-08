fb80b8911e2c85488ac7d5e1b065c10f9130423f
Faroukakindele <torchbeareroau@gmail.com>
2022-07-08 20:20:51 +0100

    new-commit

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const userName = document.getElementById("name");
 const emailAddress = document.getElementById("email");
 const warning = document.getElementById("warning");
 userName.addEventListener("keydown", (e) => {
-  e.target.value.length < 7
-    ? (warning.textContent = "Name invalid \n cannot be less than 7 characters")
-    : (warning.textContent = "Full Name valid");
+  if (e.target.value.length < 7) {
+    warning.textContent = "Name\n cannot be less than 7 characters";
+    e.target.style.borderBottom = "3px solid rgb(255, 165, 80)";
+  } else {
+    warning.textContent = "Full Name valid";
+    e.target.setAttribute("style", "borderBottom:3px solid rgb(80, 255, 118");
+  }
 });
